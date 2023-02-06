081394fb2a9cf7fb1c01be99ad6e30509a0f653f
Artem Kharkovets <Kharkovets1981@gmail.com>
2023-02-06 22:16:50 +0300

    изменен файл ДЗ6-2

--- Kharkovets_DZ№6-2.py
+++ Kharkovets_DZ№6-2.py
@@ -5,10 +5,13 @@ s1 = """this program allows you to count the number of vowels in a line"""
 s2 = "enter text: "
 print(s1.capitalize())
 s3 = input(s2)
-s4 = s3.lower()
-
-s5 = (s3.count("A")+s3.count("E")+s3.count("I")+s3.count("O")+s3.count("U")+s3.count("Y")+s3.count("a")+s3.count("e")+s3.count("i")+s3.count("o")+s3.count("u")+s3.count("y"))
-s6 = (s4.count("aeiouy"))
+s4 = s3.upper()
+s5 = (s4.count('A'))
+s6 = (s4.count('E'))
+s7 = (s4.count('I'))
+s8 = (s4.count('O'))
+s9 = (s4.count('U'))
+s10 = (s4.count('Y'))
+s11 = s5+s6+s7+s8+s9+s10
 # у цьому тексті ХХХ голосних
-print("in this text there are ", s4, " vowels")
-print("s6: in this text there are ", s6, "vowels")
+print("in this text there are ", s11, " vowels")
