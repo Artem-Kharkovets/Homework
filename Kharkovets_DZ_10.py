361c383e2b50a5256c3bfa430f52cd77b84b4619
Artem Kharkovets <Kharkovets1981@gmail.com>
2023-02-11 13:46:00 +0300

    add DZ 10-12

---
+++ Kharkovets_DZ_10.py
@@ -0,0 +1,46 @@
+# ДЗ 10. Распечатать числа Фибоначчи
+# Что такое последовательность Фибоначчи:
+# Первые два члена последовательности – ноль и один,
+# а каждый следующий – сумма значений двух предыдущих чисел:
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144.
+# Задача:
+# Пользователь вводит натуральное число n с клавиатуры.
+# Нужно для заданного числа n вывести на экран числа Фибоначчи в одну строчку,
+# где n – количество чисел в последовательности.
+# Последовательность нельзя задавать списком,
+# а нужно подсчитывать с помощью арифметики.
+# Примеры:
+# n = 1 --> 0
+# n = 5 --> 0, 1, 1, 2, 3
+# Подсказка: вам может понадобиться цикл и if
+
+#  s1 -Числа Фибоначчи.
+#  s1  fibonacci numbers.
+#  s2 -Программа отображает числа Фибоначчи с нужным вам количеством чисел в последовательности
+#  s2  The program displays the Fibonacci numbers with the number of numbers you need in the sequence
+#  s3 -Переменная "н" является количеством чисел в последовательности
+#  s3  The variable "n" is the number of numbers in the sequence
+#  s4 -Введите переменную:
+#  s4  Enter a variable:
+#  s5  n =
+#  s6 -Вы ввели НЕ натуральное число, ПЕРЕЗАПУСТИТЕ ПРОГРАММУ!
+#  s6  You have entered NOT a natural number, RESTART THE PROGRAM!
+#  s7 -результат:
+#  s7  result:
+
+
+s1 = """
+    fibonacci numbers."""
+
+s2 = """    
+    The program displays the Fibonacci numbers with
+    the number of numbers you need in the sequence.
+    The variable \"n\" is the number of numbers in the sequence.
+    Enter a variable:"""
+
+s3 = "The variable \"n\" is the number of numbers in the sequence."
+
+s4 = "Enter a variable:"
+
+print(s1.upper())
+print(s2)
