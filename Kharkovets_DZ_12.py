5cfd207a4afe1e1fc90d3b13290e38cbaf669463
Artem Kharkovets <Kharkovets1981@gmail.com>
2023-02-13 21:25:44 +0300

    DZ 10 completed

--- Kharkovets_DZ_12.py
+++ Kharkovets_DZ_12.py
@@ -13,7 +13,11 @@
 #       также вы можете выбрать любую другую букву алфавита,
 #       Программа заменит буквы нижнего регистра на верхний.
 #       Текст можно вводить со всеми грамматическими знаками.
-#  s2=  The program replaces all occurrences of the letter 'h' in the text you enter with the letter 'H', except for the first and last occurrences. You can also choose any other letter of the alphabet. The program will replace lowercase letters with uppercase ones. Text can be entered with all grammatical characters.
+#  s2=  The program replaces all occurrences of the letter 'h' in
+#       the text you enter with the letter 'H', except for the first and
+#       last occurrences. You can also choose any other letter of
+#       the alphabet. The program will replace lowercase letters with
+#       uppercase ones. Text can be entered with all grammatical characters.
 #  s3= -введите текст
 #  s3=  enter text
 #  s4= -для запуска программы выберите действие:
@@ -21,7 +25,7 @@
 #  s5= -для буквы 'h' нажмите клавишу 'enter' или введите нужную букву
 #  s5=  for the letter "h" press the "enter" key or enter the desired letter
 #  s6= -В программе нельзя вводить числа. Попробуйте еще раз!
-#  s6=
+#  s6=  You cannot enter numbers in the program. Try again!
 
 s1 = """
     replacing characters in a string"""
