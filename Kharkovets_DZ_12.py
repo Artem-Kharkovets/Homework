30d10b38b5f184ffe2875863088adc24859c18a9
Artem Kharkovets <Kharkovets1981@gmail.com>
2023-02-14 22:13:51 +0300

    DZ 12 completed

--- Kharkovets_DZ_12.py
+++ Kharkovets_DZ_12.py
@@ -1,48 +1,39 @@
-# ДЗ 12. Замена символов в строке
-# Дана строка. Замените в данной строке все вхождения буквы 'h' на букву 'H'
-# кроме первого и последнего вхождения.
-# Пример:
-# "hello hill house horse" --> "hello Hill House horse"
-# Подсказка: задачу можно решить без циклов, вам могут понадобиться срезы,
-# методы find(), rfind(), replace()
-
 #  s1= -замена символов в строке
-#  s1=  replacing characters in a string
-#  s2= -Программа заменяет в веденном вами тексте все вхождения
-#       буквы 'h' на букву 'H', кроме первого и последнего вхождения.
-#       также вы можете выбрать любую другую букву алфавита,
-#       Программа заменит буквы нижнего регистра на верхний.
-#       Текст можно вводить со всеми грамматическими знаками.
-#  s2=  The program replaces all occurrences of the letter 'h' in
-#       the text you enter with the letter 'H', except for the first and
-#       last occurrences. You can also choose any other letter of
-#       the alphabet. The program will replace lowercase letters with
-#       uppercase ones. Text can be entered with all grammatical characters.
+#  s2= -Программа заменяет все вхождения буквы «h» в тексте,
+#       который вы вводите, на букву «H» кроме первого и последнего вхождения.
+#       Текст можно вводить со всеми грамматическими символами.
 #  s3= -введите текст
-#  s3=  enter text
-#  s4= -для запуска программы выберите действие:
-#  s4=  To launch the program, select an action:
-#  s5= -для буквы 'h' нажмите клавишу 'enter' или введите нужную букву
-#  s5=  for the letter "h" press the "enter" key or enter the desired letter
-#  s6= -В программе нельзя вводить числа. Попробуйте еще раз!
-#  s6=  You cannot enter numbers in the program. Try again!
+#  s4= -Результат
 
 s1 = """
     replacing characters in a string"""
 s2 = """
-    The program replaces all occurrences of the letter 'h' in
-    the text you enter with the letter 'H', except for the first and 
-    last occurrences. You can also choose any other letter of the alphabet.
-    The program will replace lowercase letters with uppercase ones.
-    Text can be entered with all grammatical characters.
-      """
+        The program replaces all occurrences of the letter "h" in the text
+        you enter with the letter "H", except for the first and last
+        occurrence. Text can be entered with all grammar symbols.
+     """
 s3 = "enter text: "
 s4 = """
-    To launch the program, select an action:"""
-s5 = "for the letter 'h' press the 'enter' key or enter the desired letter:_"
+        Result:"""
 
 print(s1.upper())
 print(s2)
-a = input(s3)
-b = a.lower
+a1 = input(s3)
+a = a1.lower()  # без этой команды, если печатать в верхнем регистре,
+                # то 4 раза дублируется строка и меняется только
+                # последняя нужная буква или если все слова с заглавных букв,
+                # то программа тоже работает не правильно
 
+b = a.find("h")
+c = a.rfind("h")
+d = a.replace("h", "H")
+
+b1 = int(b)
+b2 = b1 + 1
+c1 = int(c)
+c2 = c1 + 1
+
+e = d[:b1] + "h" + d[b2:]
+f = e[:c1] + "h" + e[c2:]
+print(s4)
+print(f)
