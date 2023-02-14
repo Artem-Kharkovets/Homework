29bec00ac2d539113e692023bfcc6f45b4c72a93
Artem Kharkovets <Kharkovets1981@gmail.com>
2023-02-14 18:57:12 +0300

    DZ 11 redone

---
+++ Kharkovets_DZ_11_2.py
@@ -0,0 +1,125 @@
+#  s1 -последовательности целых чисел
+#  s2 -программа запрашивает ввод целых чисел, для завершения нужно ввести
+#       0 (ноль). по итогу программа выводит на экран количество, сумму и
+#       среднее арифметическое всех введенных чисел, минимальное и
+#       максимальное значение, а также количество четных и нечетных
+#       элементов последоватльности.
+#  s3 -введите число
+#  s4 -результат
+#  s5 -количество введенных чисел
+#  s6 -сумма введенных чисел
+#  s7 -среднее арифметическое всех введенных чисел
+#  s8 -минимум
+#  s9 -максимум
+#  s10 -количество четных элементов последовательности
+#  s11 -количество нечетных элементов последовательности
+#  s12 -Это не целое число, нужно ввести только целое число!
+#  s13 -Хватит дурачится, перезапусти программу!
+#  s14 -два числа... серьезно??? ну ладно
+
+s1 = """
+    sequences of integers"""
+s2 = """
+    The program asks to enter whole numbers, to complete 
+    you need to enter 0 (zero). As a result, 
+    the program displays the number, the sum and 
+    arithmetic mean of all entered numbers, 
+    the minimum and maximum values, as well as the number 
+    of even and odd elements of the sequence.
+     """
+s3 = "enter the number: "
+s4 = """
+    Result:"""
+s5 = "Number of entered numbers:"
+s6 = "Sum of entered numbers:"
+s7 = "Arithmetic mean of all entered numbers: "
+s8 = "Minimum: "
+s9 = "Maximum: "
+s10 = "Number of even sequence elements: "
+s11 = "The number of odd elements in the sequence: "
+s12 = "It's not an integer, you only need to enter an integer!"
+s13 = "Stop fooling around, restart the program!"
+s14 = "Two numbers... seriously??? OK"
+
+print(s1.upper())
+print(s2)
+
+input1 = 0  # количество ввода чисел
+si1 = 0  # сумма введенных чисел
+even1 = 0  # количество четных чисел
+odd1 = 0  # количество нечетных чисел
+max1 = 0
+min1 = 9 ** 100  # !!!!при этом значении итог будет равен нулю!!!!
+
+while True:
+    d = input(s3)
+    a = d.strip()
+    b = a[1::]
+    c = a[0]
+    if a.isdigit():
+        a1 = int(a)
+        if a1 == 0:  # если введенное число = 0, то прерывание цикла
+            input1 += 0
+            break
+        input1 += 1
+        si1 = si1 + a1
+        if a1 > max1:
+            max1 = a1
+        else:
+            max1 = max1
+        if a1 < min1:
+            min1 = a1
+        else:
+            min1 = min1
+        if a1 % 2 == 0:
+            even1 += 1
+        if a1 % 2 != 0:
+            odd1 += 1
+    else:
+        if b.isdigit() and c == "-":
+            b1 = int(b)
+            b2 = b1 * (-1)
+            if b2 == 0:  # если введенное число = 0, то прерывание цикла
+                input1 += 0
+                break
+            input1 += 1
+            si1 = si1 + b2
+            if b2 > max1:
+                max1 = b2
+            else:
+                max1 = max1
+            if b2 < min1:
+                min1 = b2
+            else:
+                min1 = min1
+            if b2 % 2 == 0:
+                even1 += 1
+            if b2 % 2 != 0:
+                odd1 += 1
+        else:
+            print(s12)
+print(s4)
+
+if input1 <= 1:
+    print(s13)
+else:
+    if input1 == 2:
+        print(s14)
+        print()
+        print(s5, input1)
+        print(s6, si1)
+        sa1 = si1 / input1  # среднее арифметическое введенных чисел
+        sa2 = round(sa1, 1)
+        print(s7, sa2)
+        print(f"minimum: {min1}; maximum: {max1}")
+        print(s10, even1)
+        print(s11, odd1)
+    else:
+        print(s5, input1)
+        print(s6, si1)
+        sa1 = si1 / input1  # среднее арифметическое введенных чисел
+        sa2 = round(sa1, 1)
+        print(s7, sa2)
+        print(f"minimum: {min1}; maximum: {max1}")
+        print(s10, even1)
+        print(s11, odd1)
