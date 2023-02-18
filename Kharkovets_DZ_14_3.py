a92a28e62f7360f08251eeecbe1df85c86fb3afe
Artem Kharkovets <Kharkovets1981@gmail.com>
2023-02-19 02:12:51 +0300

    DZ 14-3 solved with function enumerate

---
+++ Kharkovets_DZ_14_3.py
@@ -0,0 +1,42 @@
+# st1= -Сумма элементов с парным индексом из двух списков.
+# st2= -Даны два списка s1 и s2 одинакового размера, состоящих только из
+#       чисел int/float. Создать третий список s3, содержащий сумму
+#       соответствующих элементов списков s1 и s2 с одинаковым парным индексом.
+#       Вывести список s3 на экран в одну строчку.
+# st3= -Результат
+
+st1 = """
+    Sum of elements with paired index from two lists."""
+st2 = """
+Given two lists s1 and s2 of the same size, consisting only 
+of integers/floats. Create a third list s3 containing the sum of
+the corresponding elements of the lists s1 and s2 with the same 
+pairwise index. Print the list s3 to the screen in one line.
+    """
+s1 = [1, 2, 3, 4, 5]
+s2 = [4, 5, 6, 7, 8]
+s3 = []
+st3 = """
+Result:"""
+
+print(st1.upper())
+print(st2)
+print("s1 =", s1)
+print("s2 =", s2)
+
+s11 = s1[::2]
+s22 = s2[::2]
+
+print(st3, end=' ')
+for index, value in enumerate(s11):
+    b1 = index
+    c1 = value
+    for index, value in enumerate(s22):
+        b2 = index
+        c2 = value
+        if b1 == b2:
+            d = c1 + c2
+            s3.append(d)
+            print(f"{c1}+{c2}", end=', ')
+print('\b\b')
+print(s3)
